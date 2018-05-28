7c5bafd56058d99ae0f03be03e94d4c246f70509
Raj <aditya.f.raj@accenture.com>
2018-05-28 17:08:38 +0200

    image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Sprint 1</h1>
+        <h3>Individuals and interactions over processes and tools</h3>
+        <img width="300px" src="../images/Values2.jpeg" alt="Values 1 and 2"/>
+        <h3>Working Software over comprehensive documentation</h3>
+        <img width="300px" src="../images/Values1.jpeg" alt="Values 1 and 2"/>
+        <h3>Principle 2, 3, 6, 11</h3>
+        <img width="300px" src="../images/Principles.jpeg" alt="Values 1 and 2"/>
+
+        <h1>Sprint 2</h1>
+
+        <h3>Sprint</h3>
+        <img width="900px" src="../images/Sprint.png" alt="Values 1 and 2"/>
+
+        <h3>Daily Scrum</h3>
+        <img width="900px" src="../images/DailyScrum.png" alt="Values 1 and 2"/>
+
+        <h3>Sprint Planing</h3>
+        <img width="900px" src="../images/sprint-planning-large.jpg" alt="Values 1 and 2"/>
+
+        <h3>Product Backlog</h3>
+        <img width="900px" src="../images/PB.jpg" alt="Values 1 and 2"/>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// At the top of your server.js
+process.env.PWD = process.cwd()
+
+// Then
+app.use(express.static(process.env.PWD + '/public'));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
