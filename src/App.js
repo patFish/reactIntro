f608ce484915b83a5d490cec2ee94231f152b04e
Aditya Raj <adityaraj5252@gmail.com>
2018-05-29 07:20:24 +0200

    added css and buttons

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Sprint 1</h1>
-        <h3>Individuals and interactions over processes and tools</h3>
-        <img width="300px" src="../images/Values2.jpeg" alt="Values 1 and 2"/>
-        <h3>Working Software over comprehensive documentation</h3>
-        <img width="300px" src="../images/Values1.jpeg" alt="Values 1 and 2"/>
-        <h3>Principle 2, 3, 6, 11</h3>
-        <img width="300px" src="../images/Principles.jpeg" alt="Values 1 and 2"/>
 
-        <h1>Sprint 2</h1>
+    constructor() {
+        super();
+        this.state = {
+            itemsButton1: ["../images/Values2.jpeg", "../images/Values1.jpeg", "../images/Principles.jpeg"],
+            headingButton1: ["Individuals and interactions over processes and tools",
+                "Working Software over comprehensive documentation",
+                "Principle 2, 3, 6, 11"],
+            isHiddenitem1: true,
 
-        <h3>Sprint</h3>
-        <img width="900px" src="../images/Sprint.png" alt="Values 1 and 2"/>
+            itemsButton2: ["../images/Sprint.png", "../images/DailyScrum.png", "../images/sprint-planning-large.jpg", "../images/PB.jpg"],
+            headingButton2: ["Sprint", "Daily Scrum", "Sprint Planing", "Product Backlog"],
 
-        <h3>Daily Scrum</h3>
-        <img width="900px" src="../images/DailyScrum.png" alt="Values 1 and 2"/>
+            button1counter: 0,
+            button2counter: 0,
+            imageWidth: 900,
+            isHiddenitem2: true,
+        };
+        this.onButtonClick1 = this.onButtonClick1.bind(this);
+        this.onButtonClick2 = this.onButtonClick2.bind(this);
+        this.onIncreaseImageWidth = this.onIncreaseImageWidth.bind(this);
+    }
 
-        <h3>Sprint Planing</h3>
-        <img width="900px" src="../images/sprint-planning-large.jpg" alt="Values 1 and 2"/>
+    onButtonClick1() {
+        const button1counterstate = this.state.button1counter;
+        this.setState({
+            button1counter: (button1counterstate + 1) % this.state.headingButton1.length,
+            isHiddenitem1: false,
+            isHiddenitem2: true,
+        });
 
-        <h3>Product Backlog</h3>
-        <img width="900px" src="../images/PB.jpg" alt="Values 1 and 2"/>
-      </div>
-    );
-  }
+    }
+
+    onButtonClick2() {
+        const button2counterstate = this.state.button2counter;
+        this.setState({
+            button2counter: (button2counterstate + 1) % this.state.headingButton2.length,
+            isHiddenitem1: true,
+            isHiddenitem2: false,
+        });
+    }
+
+    onIncreaseImageWidth(){
+        const currentImageWidth = this.state.imageWidth;
+        this.setState({
+            imageWidth: (currentImageWidth+10)%1024,
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <input type="button" className="btn btn-lg float-right" onClick={this.onIncreaseImageWidth} value="Increase Image Dimensions"/>
+                    <h2>Welcome to React </h2>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                <input type="button" className="btn btn-danger" onClick={this.onButtonClick1} value="Sprint 1"/>
+                            </div>
+                            <div className="col-6">
+                                <input type="button" className="btn btn-danger" onClick={this.onButtonClick2} value="Sprint 2"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        {!this.state.isHiddenitem1 &&
+                        <div className="col-6">
+                            {/*<input type="button" className="btn btn-primary" onClick={this.onButtonClick1} value="Sprint 1"/>*/}
+                            <h3><br/><br/><br/><br/>{this.state.headingButton1[this.state.button1counter]}</h3>
+                            <img width={this.state.imageWidth} src={this.state.itemsButton1[this.state.button1counter]} alt="Values 1 and 2"/>
+                        </div>}
+                        {!this.state.isHiddenitem2 &&
+                        <div className="col-6">
+                            {/*<input type="button" className="btn btn-primary" onClick={this.onButtonClick2} value="Sprint 2"/>*/}
+                            <h3><br/><br/><br/><br/>{this.state.headingButton2[this.state.button2counter]}</h3>
+                            <img width={this.state.imageWidth} src={this.state.itemsButton2[this.state.button2counter]} alt="Values 1 and 2"/>
+                        </div> }
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
