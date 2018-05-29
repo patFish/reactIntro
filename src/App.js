248b08aec2fed72369a78e578a6014777dd2b082
Eric Zuchantke <eric.zuchantk@gmail.com>
2018-05-29 09:25:49 +0200

    Added div to App.js for better ordering

--- src/App.js
+++ src/App.js
@@ -10,39 +10,43 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React!</h2>
         </div>
-        
-        <h1>Product Owner</h1>
-        <img src={require("./img/productOwner.jpeg")}/>
-        <h1>Customer Collaboration over contract negotiation</h1>
-        <img className="rotate_plus_90" src={require("./img/value3.jpeg")}/>
-        <h1>Responding to change over following a plan</h1>
-        <img className="rotate_180"src={require("./img/value4.jpeg")}/>
-        <h1>1. Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.</h1>
-        <img className="rotate_plus_90" src={require("./img/principle1.jpeg")}/>
-        <h1>4. Business people and developers must work together daily throughout the project.</h1>
-        <img src={require("./img/principle4.jpeg")}/>
-        <h1>5. Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.</h1>
-        <img src={require("./img/principle5.jpeg")}/>
-        <h1>7. Working software is the primary measure of progress.</h1>
-        <img src={require("./img/principle7.jpeg")}/>
-        <h1>8. Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.</h1>
-        <img className="rotate_plus_90" src={require("./img/principle8.jpeg")}/>
-        <h1>9. Continuous attention to technical excellence and good design enhances agility.</h1>
-        <img className="rotate_180" src={require("./img/principle9.jpeg")}/>
-        <h1>10. Simplicity -- the art of maximizing the amount of work not done -- is essential.</h1>
-        <img src={require("./img/principle10.jpeg")}/>
-        <h1>12. At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.</h1>
-        <img src={require("./img/principle12.jpeg")}/>
 
-        <div id="scrum-div">
-          <h1>Sprint review</h1>
-          <img src={require("./img/SprintReview.jpeg")}/>
-          <h1>Sprint Retro</h1>
-          <img src={require("./img/Sprint Retrospective.jpeg")}/>
-          <h1>Sprint Backlog</h1>
-          <img src={require("./img/Sprint_Backlog.jpg")}/>
-          <h1>Increment</h1>
-          <img src={require("./img/SCRUM_Artifact_Increment.jpeg")}/>
+        <div id="sprints-div">
+          <div id="Agile-div">
+            <h1>Product Owner</h1>
+            <img src={require("./img/productOwner.jpeg")}/>
+            <h1>Customer Collaboration over contract negotiation</h1>
+            <img className="rotate_plus_90" src={require("./img/value3.jpeg")}/>
+            <h1>Responding to change over following a plan</h1>
+            <img className="rotate_180"src={require("./img/value4.jpeg")}/>
+            <h1>1. Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.</h1>
+            <img className="rotate_plus_90" src={require("./img/principle1.jpeg")}/>
+            <h1>4. Business people and developers must work together daily throughout the project.</h1>
+            <img src={require("./img/principle4.jpeg")}/>
+            <h1>5. Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.</h1>
+            <img src={require("./img/principle5.jpeg")}/>
+            <h1>7. Working software is the primary measure of progress.</h1>
+            <img src={require("./img/principle7.jpeg")}/>
+            <h1>8. Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.</h1>
+            <img className="rotate_plus_90" src={require("./img/principle8.jpeg")}/>
+            <h1>9. Continuous attention to technical excellence and good design enhances agility.</h1>
+            <img className="rotate_180" src={require("./img/principle9.jpeg")}/>
+            <h1>10. Simplicity -- the art of maximizing the amount of work not done -- is essential.</h1>
+            <img src={require("./img/principle10.jpeg")}/>
+            <h1>12. At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.</h1>
+            <img src={require("./img/principle12.jpeg")}/>
+          </div>
+
+          <div id="scrum-div">
+            <h1>Sprint review</h1>
+            <img src={require("./img/SprintReview.jpeg")}/>
+            <h1>Sprint Retro</h1>
+            <img src={require("./img/Sprint Retrospective.jpeg")}/>
+            <h1>Sprint Backlog</h1>
+            <img src={require("./img/Sprint_Backlog.jpg")}/>
+            <h1>Increment</h1>
+            <img src={require("./img/SCRUM_Artifact_Increment.jpeg")}/>
+          </div>
         </div>
       </div>
     );
