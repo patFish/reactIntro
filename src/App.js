7b403263596dcf61a30f6f73485b1ee29db19988
Eric Zuchantke <eric.zuchantk@gmail.com>
2018-05-30 10:31:15 +0200

    Create folder for next sprint and rearange app.js

--- src/App.js
+++ src/App.js
@@ -12,42 +12,42 @@ class App extends Component {
         </div>
 
         <div id="sprint1">
-        <div id="sprints-div" className="sprint">
-          <h1>Sprint 1</h1>
-          <div id="Agile-div">
-            <h1>Product Owner</h1>
-            <img src={require("./img/productOwner.jpeg")}/>
-            <h1>Individuals over processes and tools</h1>
-            <img src={require("./img/Values2.jpeg")}/>
-            <h1>Working software over comprehensive documentation</h1>
-            <img src={require("./img/Values1.jpeg")}/>
-            <h1>Customer Collaboration over contract negotiation</h1>
-            <img src={require("./img/value3.jpeg")}/>
-            <h1>Responding to change over following a plan</h1>
-            <img src={require("./img/value4.jpeg")}/>
-            <h1>Principles of Agile 2, 3, 6, 11</h1>
-            <img className="rotate_minus_90" src={require("./img/Principles.jpeg")}/>
-            <h1>1. Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.</h1>
-            <img src={require("./img/principle1.jpeg")}/>
-            <h1>4. Business people and developers must work together daily throughout the project.</h1>
-            <img src={require("./img/principle4.jpeg")}/>
-            <h1>5. Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.</h1>
-            <img src={require("./img/principle5.jpeg")}/>
-            <h1>7. Working software is the primary measure of progress.</h1>
-            <img src={require("./img/principle7.jpeg")}/>
-            <h1>8. Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.</h1>
-            <img src={require("./img/principle8.jpeg")}/>
-            <h1>9. Continuous attention to technical excellence and good design enhances agility.</h1>
-            <img src={require("./img/principle9.jpeg")}/>
-            <h1>10. Simplicity -- the art of maximizing the amount of work not done -- is essential.</h1>
-            <img src={require("./img/principle10.jpeg")}/>
-            <h1>12. At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.</h1>
-            <img src={require("./img/principle12.jpeg")}/>
+          <div id="sprints-div" className="sprint">
+            <h1>Sprint 1</h1>
+            <div id="Agile-div">
+              <h1>Product Owner</h1>
+              <img src={require("./img/productOwner.jpeg")}/>
+              <h1>Individuals over processes and tools</h1>
+              <img src={require("./img/Values2.jpeg")}/>
+              <h1>Working software over comprehensive documentation</h1>
+              <img src={require("./img/Values1.jpeg")}/>
+              <h1>Customer Collaboration over contract negotiation</h1>
+              <img src={require("./img/value3.jpeg")}/>
+              <h1>Responding to change over following a plan</h1>
+              <img src={require("./img/value4.jpeg")}/>
+              <h1>Principles of Agile 2, 3, 6, 11</h1>
+              <img className="rotate_minus_90" src={require("./img/Principles.jpeg")}/>
+              <h1>1. Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.</h1>
+              <img src={require("./img/principle1.jpeg")}/>
+              <h1>4. Business people and developers must work together daily throughout the project.</h1>
+              <img src={require("./img/principle4.jpeg")}/>
+              <h1>5. Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.</h1>
+              <img src={require("./img/principle5.jpeg")}/>
+              <h1>7. Working software is the primary measure of progress.</h1>
+              <img src={require("./img/principle7.jpeg")}/>
+              <h1>8. Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.</h1>
+              <img src={require("./img/principle8.jpeg")}/>
+              <h1>9. Continuous attention to technical excellence and good design enhances agility.</h1>
+              <img src={require("./img/principle9.jpeg")}/>
+              <h1>10. Simplicity -- the art of maximizing the amount of work not done -- is essential.</h1>
+              <img src={require("./img/principle10.jpeg")}/>
+              <h1>12. At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.</h1>
+              <img src={require("./img/principle12.jpeg")}/>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
 
-          <div id="sprint2">
+        <div id="sprint2">
           <h1>Sprint 2</h1>
           <div id="scrum-div" className="sprint">
             <h1>Sprint</h1>
@@ -67,9 +67,9 @@ class App extends Component {
             <h1>Increment</h1>
             <img src={require("./img/SCRUM_Artifact_Increment.jpeg")}/>
           </div>
-          </div>
+        </div>
 
-          <div id="sprint3">
+        <div id="sprint3">
           <h1>Sprint 3</h1>
           <div id="sprintbacklog-div" className="sprint">
             <h1>Product Backlog Item</h1>
@@ -90,9 +90,9 @@ class App extends Component {
             <h1>Scrum Task</h1>
             <img src={require("./img/Scrum-Task.jpg")}/>
           </div>
-          </div>
+        </div>
 
-          <div id="sprint4">
+        <div id="sprint4">
           <h1>Sprint 4</h1>
           <div id="workload-div" className="sprint">
             <h1>Communicate work: assess progress</h1>
@@ -109,7 +109,9 @@ class App extends Component {
             <h1>Branching</h1>
             <img src={require("./img/Day2/Sprint3/Agile.png")}/>
           </div>
+        </div>
 
+        <div id="sprint5">
           <h1>Sprint 5</h1>
           <div id="merging-div" className="sprint">
             <h1>Dev Test and Practices</h1>
@@ -127,8 +129,7 @@ class App extends Component {
             <h1>Code_Coverage</h1>
             <img src={require("./img/Sprint5/Code_Coverage.jpg")}/>
           </div>
-
-          </div>
+        </div>
 
         </div>
       
